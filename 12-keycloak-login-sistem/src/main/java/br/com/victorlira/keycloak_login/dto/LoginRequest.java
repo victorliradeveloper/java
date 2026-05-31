cef9df5a7e5ad018962f877f8a1778717889fc1b
Victor Lira <victorliradatum@gmail.com>
2026-05-31 15:08:28 -0300

    remove: key cloak

--- 12-keycloak-login-sistem/src/main/java/br/com/victorlira/keycloak_login/dto/LoginRequest.java
+++
@@ -1,3 +0,0 @@
-package br.com.victorlira.keycloak_login.dto;
-
-public record LoginRequest(String username, String password) {}
--- 12-keycloak-login-sistem/src/main/java/br/com/victorlira/keycloak_login/dto/RegisterRequest.java
+++
@@ -1,9 +0,0 @@
-package br.com.victorlira.keycloak_login.dto;
-
-public record RegisterRequest(
-        String username,
-        String email,
-        String firstName,
-        String lastName,
-        String password
-) {}
